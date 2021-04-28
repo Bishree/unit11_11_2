a76f7848f85bf1783c7d0908a8aaf2af87642c5e
Bishree <ahmedbishree@gmail.com@users.noreply.github.com>
2021-04-28 23:59:20 +0300

    _last_commit_unit_test

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,26 @@
+pub fn print_and_return_10 (a:i32) -> i32 {
+    println!("I've got the value {}", a);
+    10
+}
+
 #[cfg(test)]
 mod tests {
+    use crate::print_and_return_10;
+    use super::*;
+
     #[test]
+    fn this_test_will_pass (){
+        let value = print_and_return_10(4);
+        assert_eq!(10, value);
+    }
+    #[test]
+    fn this_test_will_fail () {
+        let value = print_and_return_10(8);
+        assert_eq!(5 , value);
+    }
+
+    #[test]
+    #[ignore]
     fn it_works() {
         assert_eq!(2 + 2, 4);
     }
